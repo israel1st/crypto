12dc8a24304721282ae89abdf5e6d343f4372fbc
Dimkpa-Daniel <danieldimmkpa1@gmail.com>
2023-04-14 16:50:47 +0100

    first commit

---
+++ server/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: "mongodb+srv://israelbasseyib:hIVzqbI5J1Ty94Eo@cluster0.5bctdrq.mongodb.net/test",
+  secretOrKey: "secret",
+  SENDGRID_API: "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"
+};
